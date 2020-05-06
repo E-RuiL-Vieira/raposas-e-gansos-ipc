d0f20a378fa3d0af572f8116e285cf66701bb5f4
isikawalia <64701554+isikawalia@users.noreply.github.com>
2020-05-06 12:10:22 -0300

    Add files via upload

---
+++ raposa_e_gansos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    //imprime a configurção inicial
+    printf("                         \n");
+    printf("        o---o---o        \n");
+    printf("        |   |   |        \n");
+    printf("        |   |   |        \n");
+    printf("        o---o---o        \n");
+    printf("        |   |   |        \n");
+    printf("        |   |   |        \n");
+    printf("A---o---o---Z---o---o---K\n");
+    printf("|   |   |   |   |   |   |\n");
+    printf("|   |   |   |   |   |   |\n");
+    printf("B---o---o---o---o---o---J\n");
+    printf("|   |   |   |   |   |   |\n");;
+    printf("|   |   |   |   |   |   |\n");
+    printf("C---D---E---F---G---H---I\n");
+    printf("        |   |   |        \n");
+    printf("        |   |   |        \n");
+    printf("        L---M---N        \n");
+    printf("        |   |   |        \n");
+    printf("        |   |   |        \n");
+    printf("        O---P---Q        \n");
+
+
+return(0);
+}
