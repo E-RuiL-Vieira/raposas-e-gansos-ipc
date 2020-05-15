55b9c9583045b9249e620b81d4020e10b5054221
E-RuiL-Vieira <64679344+E-RuiL-Vieira@users.noreply.github.com>
2020-05-15 17:02:27 -0300

    Update raposa_e_gansos.c

--- raposa_e_gansos.c
+++ raposa_e_gansos.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    char m[7][7];
+void imprimir(char tab[7][7]){ //Imprime o tabuleiro de uma forma agradável ao usuário
+	int i, j;
+	for (i= 0; i < 7; i++){ 
+		for (j = 0; j < 7; j++){ 
+			if(tab[i][j] == ' '){
+				printf("%c   ", tab[i][j]);
+				continue;
+			}
+			printf("%c", tab[i][j]);
+			if (j != 6){
+				printf("---");
+			}
+		}
 
-    //imprime a configurção inicial
-    printf("        o---o---o        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        o---o---o        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("A---o---o---Z---o---o---K\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("B---o---o---o---o---o---J\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("C---D---E---F---G---H---I\n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        L---M---N        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        O---P---Q        \n");
-    
-    //tabuleiro pra imprimir as jogadas    
-    printf("        %c---%c---%c        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        %c---%c---%c        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("%c---%c---%c---%c---%c---%c---%c\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("%c---%c---%c---%c---%c---%c---%c\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("|   |   |   |   |   |   |\n");
-    printf("%c---%c---%c---%c---%c---%c---%c\n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        %c---%c---%c        \n");
-    printf("        |   |   |        \n");
-    printf("        |   |   |        \n");
-    printf("        %c---%c---%c        \n");
+		printf("\n\n\n");
+	}
+}
+int casaehvalida(char casa){ //Verifica se a casa do movimento selecionado está livre e faz parte do tabuleiro
+	if (casa != 'o'){ 
+		return -1;
+	}
+	return 1;
+}
+int temdiagonal(int i, int j){ //Verifica se a casa atual permite movimentos diagonais
+	if (i == 0 || i == 2 || i == 4 || i == 6){ 
+		if (j % 2 == 0){ //Em casas de linha par (começando a partir de zero), colunas pares permitem movimento diagonal
+			return 1; //Permite movimento diagonal
+		}
+		return -1; //Não permite movimento diagonal
+	}
+	else{ 
+		if (j % 2 != 0){ //Em casas de linha ímpar, colunas ímpares permitem movimento diagonal 
+			return 1; //Permite movimento diagonal
+		}
+		return -1; //Não permite movimento diagonal
+	}
+}
 
 
+int main(void) {
+  char m[7][7] = {{"  ooo  "}, {"  ooo  "}, {"AooZooK"}, {"BoooooJ"}, {"CDEFGHI"}, {"  LMN  "}, {"  OPQ  "}}; //Configuração Inicial
+	imprimir(m);
 
-
-return(0);
+  return 0;
 }
